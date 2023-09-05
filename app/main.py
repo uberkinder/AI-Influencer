c9965aae18ef59c470fd27aa51845816b8110d84
sir-timio <t-ionov@list.ru>
2023-09-05 12:29:34 +0300

    add simplt create functionality

--- app/main.py
+++ app/main.py
@@ -5,8 +5,21 @@ import os
 import sys
 from datetime import datetime, timedelta
 
-from aiogram import Bot, Dispatcher, types
-from aiogram.filters import Command
+import yaml
+from aiogram import Bot, Dispatcher, F, Router, types
+from aiogram.enums import ParseMode
+from aiogram.filters import Command, CommandStart
+from aiogram.fsm.context import FSMContext
+from aiogram.types import (
+    KeyboardButton,
+    Message,
+    ReplyKeyboardMarkup,
+    ReplyKeyboardRemove,
+)
+
+from form import UserForm
+
+sys.path.append("../../")
 from src import utils
 from src.chatgpt import post, reply
 
@@ -20,9 +33,8 @@ utils.load_secrets("config/.env")
 # Initialize Bot and dispatcher
 bot = Bot(token=os.getenv("TG_TOKEN"))
 dp = Dispatcher()
-
-chat_schedules = {}
-
+form_router = Router()
+dp.include_router(form_router)
 
 chat_schedules = {}
 
@@ -88,28 +100,89 @@ async def set_user_schedule(message: types.Message):
     await message.reply(f"Schedule set for times: {', '.join(time_list)}")
 
 
-async def on_startup(dp):
-    """Set bot commands"""
-    # Add the /start command to the list of bot commands
-    logger.info("Bot is starting up...")
+@dp.message(Command("start"))
+async def start_bot(message: types.Message):
+    welcome_message = (
+        "Welcome to the bot! 🤖\n\n"
+        "Here's what you can do:\n"
+        "- Use /create to answer a few questions and create your profile.\n\n"
+        "- Use /set_schedule to set a schedule. E.g.,\n `/set_schedule 09:00 12:30 18:45`\n\n"
+        "Type any message to continue."
+    )
+
+    await message.reply(welcome_message)
 
 
-# Handler for solo messages
-@dp.message()
-async def echo_handler(message: types.Message):
-    """This handler will be called when user sends a direct message"""
-    try:
-        message_text = message.text
-        logger.info("User direct message:\n" + message_text)
-        response = reply(message_text, config=cfg)
-        logger.info("ChatGPT response:\n" + response)
-        await message.reply(response)
+@dp.message(Command("create"))
+async def command_start(message: Message, state: FSMContext) -> None:
+    await state.set_state(UserForm.name)
+    await message.answer(
+        "Hi there! What's your name?",
+        reply_markup=ReplyKeyboardRemove(),
+    )
 
-    except Exception as e:
-        dev = cfg["developers"]
-        msg = f"Sorry, I have internal problems. Please contact {dev}"
-        await message.reply(msg)
-        logger.error(e)
+
+# @form_router.message(Command("cancel"))
+# @form_router.message(F.text.casefold() == "cancel")
+# async def cancel_handler(message: Message, state: FSMContext) -> None:
+#     """
+#     Allow user to cancel any action
+#     """
+#     current_state = await state.get_state()
+#     if current_state is None:
+#         return
+
+#     logging.info("Cancelling state %r", current_state)
+#     await state.clear()
+#     await message.answer(
+#         "Cancelled.",
+#         reply_markup=ReplyKeyboardRemove(),
+#     )
+
+
+@form_router.message(UserForm.name)
+async def command_enter_name(message: Message, state: FSMContext) -> None:
+    user_name = message.text
+    print()
+    await state.update_data(name=user_name)  # Save name
+    # await UserForm.next()  # Go to the next state
+    await state.set_state(UserForm.age)
+    await message.answer("Great! How old are you?")
+
+
+@form_router.message(UserForm.age)
+async def command_enter_age(message: Message, state: FSMContext) -> None:
+    user_age = message.text  # assume the user enters a number
+    await state.update_data(age=user_age)  # Save age
+    data = await state.get_data()
+    await state.clear()  # Clear the state
+
+    await message.answer(
+        f"Great, here is what you told me:\n"
+        f"- Name: {data['name']}\n"
+        f"- Age: {data['age']}\n"
+    )
+    chat_id = message.chat.id
+    with open(f"data/{chat_id}.yml", "w") as f:
+        yaml.dump(data, f, default_flow_style=False)
+
+
+# # Handler for solo messages
+# @dp.message()
+# async def echo_handler(message: types.Message):
+#     """This handler will be called when user sends a direct message"""
+#     try:
+#         message_text = message.text
+#         logger.info("User direct message:\n" + message_text)
+#         response = reply(message_text, config=cfg)
+#         logger.info("ChatGPT response:\n" + response)
+#         await message.reply(response)
+
+#     except Exception as e:
+#         dev = cfg["developers"]
+#         msg = f"Sorry, I have internal problems. Please contact {dev}"
+#         await message.reply(msg)
+#         logger.error(e)
 
 
 async def main() -> None:
