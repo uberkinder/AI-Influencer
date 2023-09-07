56d6ea31b6a473292ff3459ff50287881b6af573
da-kartushov <kartyshov.d@team.karpov.courses>
2023-09-07 20:04:19 +0300

    mvp

--- app/main.py
+++ app/main.py
@@ -3,10 +3,16 @@ import logging
 import os
 import sys
 
-#sys.path.append(os.path.abspath(os.path.dirname(__file__) + "/.."))
+import aioschedule as schedule
 from aiogram import Bot, Dispatcher, types
 from src import utils
-from src.chatgpt import post, reply
+from src.chatgpt import post, reply, summarize_in_batches
+from src.prompts import (
+    prompt_reply,
+    prepromt,
+    prompt_post,
+    prompt_post_from_reply_direct,
+)
 
 # Logging
 logger = logging.getLogger("AI-influencer")
@@ -19,53 +25,63 @@ utils.load_secrets("config/.env")
 bot = Bot(token=os.getenv("TG_TOKEN"))
 dp = Dispatcher()
 
-TIME = 30
+TIME = 60 * 60 * 12
 
 
 async def send_periodic_message():
+    try:
+        post_about_shed = "Write random post about random topic based on your experience and science facts in AI, Machine Learning or Productivity"
+        # post_text = summarize_in_batches(channel_post.reply_to_message.text, config=cfg)
+        prepromt_text = prepromt()
+        prompt_post_text = prompt_post(prepromt_text, post_about_shed)
+        post_text = post(prompt_post_text, config=cfg)
+        await bot.send_message(
+            chat_id=os.getenv("TG_CHANNEL_ID"),
+            text=post_text,
+            parse_mode="Markdown",
+        )
+    except Exception as e:
+        logger.error(f"Error sending message: {e}")
+
+
+async def scheduler():
+    schedule.every(TIME).seconds.do(send_periodic_message)
     while True:
-        try:
-            # new_post = post(
-            #    "Generate a new post on LLMs in your Telegram channel", config=cfg
-            # )
-            new_post = "Глушилка на шедулер"
-            logger.info("SHEDDULING MESSAGE \n" + new_post)
-            await bot.send_message(
-                chat_id=os.getenv("TG_CHANNEL_ID"),
-                text=new_post,
-            )
-            await asyncio.sleep(TIME)
-        except Exception as e:
-            logger.error("Error sending message: {e}")
-            await asyncio.sleep(TIME)
+        await schedule.run_pending()
+        await asyncio.sleep(TIME)
 
 
 async def on_startup(dp):
     """Set bot commands"""
-    # Add the /start command to the list of bot commands
     logger.info("Bot is starting up...")
-    asyncio.create_task(send_periodic_message())  # Start the repeated task
+    # asyncio.create_task(send_periodic_message())
+    # asyncio.create_task(scheduler())
 
 
 @dp.message(lambda message: message.reply_to_message is not None)
 async def log_channel_comments(channel_post: types.Message):
     if channel_post.reply_to_message:
-        # Если это ответ на сообщение из канала
         if (
             channel_post.reply_to_message.sender_chat
             and channel_post.reply_to_message.sender_chat.type == "channel"
         ):
             logger.info(
-                f"Reply from {channel_post.from_user.username or channel_post.from_user.id} to channel message {channel_post.reply_to_message.text}: {channel_post.text}"
+                f"""Reply from {channel_post.from_user.username or channel_post.from_user.id} 
+                To channel message {channel_post.reply_to_message.text}: {channel_post.text}"""
             )
             try:
+                prepromt_text = prepromt()
                 user_message = channel_post.text
-                chatgpt_response = reply(user_message, config=cfg)
-                #chatgpt_response = "Заглушка на реплай"
+                # post_text = summarize_in_batches(channel_post.reply_to_message.text, config=cfg)
+                post_text = channel_post.reply_to_message.text
+                username = channel_post.from_user.username
+                prompt = prompt_reply(prepromt_text, user_message, post_text, username)
+                chatgpt_response = reply(prompt, config=cfg)
                 await bot.send_message(
                     chat_id=channel_post.chat.id,
                     text=chatgpt_response,
                     reply_to_message_id=channel_post.message_id,
+                    parse_mode="Markdown",
                 )
             except Exception as e:
                 dev = cfg["developers"]
@@ -76,15 +92,51 @@ async def log_channel_comments(channel_post: types.Message):
                 logger.error(e)
 
 
-@dp.message()
+@dp.message(
+    lambda message: message.forward_from_chat is not None
+    and message.chat.type == "private"
+)
+async def forwarded_message_handler(message: types.Message):
+    try:
+        prepromt_text = prepromt()
+        channel_name = message.forward_from_chat.title
+        channel_id = message.forward_from_chat.username
+
+        logger.info(
+            f"FUNC forwarded_message_handler\n Reply from {channel_name}: @{channel_id}"
+        )
+        message_text = message.caption
+
+        logger.info("User direct message:\n" + message_text)
+        prompt_text = prompt_post_from_reply_direct(
+            prepromt_text, message_text, channel_name, channel_id
+        )
+        new_post = post(prompt_text, config=cfg)
+
+        logger.info("Post message:\n" + new_post)
+        await bot.send_message(
+            chat_id=os.getenv("TG_CHANNEL_ID"),
+            text=new_post[:4096],
+            parse_mode="Markdown",
+        )
+    except Exception as e:
+        dev = cfg["developers"]
+        msg = f"Sorry, I have internal problems. Please contact {', '.join(dev)}"
+        await message.reply(msg)
+        logger.error(e)
+
+
+@dp.message(
+    lambda message: message.forward_from_chat is None and message.chat.type == "private"
+)
 async def post_dir2blog(message: types.Message):
     """This handler will be called when user sends a direct message"""
     if message.chat.type != "private":
         return
 
     try:
-        message_text = message.text        
-        logger.info("User direct message:\n" + message_text)
+        message_text = message.text
+        logger.info("FUNC `post_dir2blog`\n User direct message:\n" + message_text)
 
         new_post = post(message_text, config=cfg)
         # new_post = "Глушилка на реплай из лички боту"
@@ -92,11 +144,8 @@ async def post_dir2blog(message: types.Message):
         await bot.send_message(
             chat_id=os.getenv("TG_CHANNEL_ID"),
             text=new_post[:4096],
+            parse_mode="Markdown",
         )
-        # await bot.send_message(
-        #     chat_id=os.getenv("TG_CHANNEL_ID"),
-        #     text=new_post[4096:],
-        # )
     except Exception as e:
         dev = cfg["developers"]
         msg = f"Sorry, I have internal problems. Please contact {', '.join(dev)}"
@@ -105,6 +154,7 @@ async def post_dir2blog(message: types.Message):
 
 
 async def main() -> None:
+    await on_startup(dp)
     await dp.start_polling(bot)
 
 
--- src/prompts.py
+++ src/prompts.py
@@ -10,7 +10,7 @@ def prepromt(prompt: str = None) -> str:
     - DateTime: 12:20:30 november 2022
     - AnswerStyle: like a human, non-formal, telegram or social network user
     """
-    if prompt == None:
+    if prompt is None:
         prompt = f"""
     - Name: I Am Not Human
     - Niche: Machine Learning, Artificial Intelligence
@@ -18,20 +18,28 @@ def prepromt(prompt: str = None) -> str:
     - Age: 26 years old
     - DateTime: {datetime.now()}
     - AnswerStyle: like a human, non-formal, telegram or social network user
-        """
+    """
     return prompt
 
 
-def prompt_reply(preprompt: str, user_message: str, blog_post: str) -> str:
+def prompt_reply(
+    preprompt: str, user_message: str, blog_post: str, username: str
+) -> str:
     prompt = f"""
     You are the author of blog
     {preprompt}
 
-    A reader has left the following comment on your post:
+    A reader has left the following comment on your post from user {username}:
     -```{user_message}```
 
     How would you respond to this comment as the author of the following blog post?:
     -```{blog_post}```
+
+    Text Formatting:
+    - Write like a human in social network
+    - Use emotions and emojis in the text
+    - Use text highlighting for headers and subheaders
+    - Make the text structured
     """
     return prompt
 
@@ -46,6 +54,57 @@ def prompt_post(
 
     Output Information:
     - ```{blog_post}```
+
+    Text Formatting:
+    - Write like a human
+    - Use emotions and emojis in the text
+    - Avoid bureaucratic language
+    - Use text highlighting for headers and subheaders
+    - Make the text structured
+    - At the end, add 3-4 keywords related to the text topic with #, for example, for a travel topic #travel
+    - Do not use words like "Header", "Keywords", etc.
+    """
+    return prompt
+
+
+def prompt_post_from_reply(
+    preprompt,
+    blog_post: str = "An interesting post on a random topic based on your experience",
+    authorname: str = ""
+) -> str:
+    prompt = f"""
+    You are the author of blog
+    {preprompt}
+
+    Provide a review on this post:
+    - ```{blog_post}```
+    - Author of this post is {authorname}
+
+    Text Formatting:
+    - Write like a human
+    - Use emotions and emojis in the text
+    - Avoid bureaucratic language
+    - Use text highlighting for headers and subheaders
+    - Make the text structured
+    - At the end, add 3-4 keywords related to the text topic with #, for example, for a travel topic #travel
+    - Do not use words like "Header", "Keywords", etc.
+    """
+    return prompt
+
+
+def prompt_post_from_reply_direct(
+    preprompt,
+    blog_post: str = "An interesting post on a random topic based on your experience",
+    authorname: str = "",
+    id_author: str = "",
+) -> str:
+    prompt = f"""
+    You are the author of blog
+    {preprompt}
+
+    Provide a review on this post:
+    - ```{blog_post}```
+    - Author of this post is {authorname} with telegram id @{id_author}
 
     Text Formatting:
     - Write like a human
